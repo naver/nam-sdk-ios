1dc7d3c3ef3a074fecaded4d1a86ff5d62e55706
namsdk-ios-distribution <gfpsdk.dev@navercorp.com>
2025-06-05 14:42:08 +0900

    [NAMSDK] Update 8.6.1 Version.

--- NAMSDK/xcframework/GFPSDK.xcframework/ios-arm64/GFPSDK.framework/Headers/GFPCoreConfig.h
+++ NAMSDK/xcframework/GFPSDK.xcframework/ios-arm64/GFPSDK.framework/Headers/GFPCoreConfig.h
@@ -26,7 +26,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-#define GFP_SDK_VERSION              @"8.6.0"
+#define GFP_SDK_VERSION              @"8.6.1"
 
 @interface GFPCoreConfig : NSObject <GFPWebViewAppEnvDelegate>
 
