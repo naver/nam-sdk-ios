be163aed28ff61e5fd3740dc3bd4ff5eb236cbd9
namsdk-ios-distribution <gfpsdk.dev@navercorp.com>
2026-04-14 17:48:26 +0900

    [MediationSDK] Update FAN 6.20.1.1 Version.

---
+++ MediationSDK/FAN/6.20.1.1/GFPSDKMediationFAN.xcframework/ios-arm64_x86_64-simulator/GFPSDKMediationFAN.framework/Headers/GFPSDKMediationFAN.h
@@ -0,0 +1,19 @@
+//
+//  GFPSDKMediationFAN.h
+//
+//  NAM(Naver Ad Manager) SDK for iOS
+//  Copyright 2022-present NAVER Corp.
+//  Unauthorized use, modification and redistribution of this software are strongly prohibited.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GFPSDKMediationFAN.
+FOUNDATION_EXPORT double GFPSDKMediationFANVersionNumber;
+
+//! Project version string for GFPSDKMediationFAN.
+FOUNDATION_EXPORT const unsigned char GFPSDKMediationFANVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GFPSDKMediationFAN/PublicHeader.h>
+
+
