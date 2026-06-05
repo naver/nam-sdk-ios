aea942b893ae9f0f70d33edae2892be7e3b24e0f
namsdk-ios-distribution <gfpsdk.dev@navercorp.com>
2026-06-05 14:00:36 +0900

    [AdMediationSDK] sandbox 0.0.1.0 (flat structure baseline)

---
+++ AdMediationSDK/IMA/GFPSDKMediationIMA.xcframework/ios-arm64_x86_64-simulator/GFPSDKMediationIMA.framework/Headers/GFPSDKMediationIMA.h
@@ -0,0 +1,19 @@
+//
+//  GFPSDKMediationIMA.h
+//
+//  NAM(Naver Ad Manager) SDK for iOS
+//  Copyright 2022-present NAVER Corp.
+//  Unauthorized use, modification and redistribution of this software are strongly prohibited.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GFPSDKMediationIMA.
+FOUNDATION_EXPORT double GFPSDKMediationIMAVersionNumber;
+
+//! Project version string for GFPSDKMediationIMA.
+FOUNDATION_EXPORT const unsigned char GFPSDKMediationIMAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GFPSDKMediationIMA/PublicHeader.h>
+
+
---
+++ AdMediationSDK/LAN/GFPSDKMediationLAN.xcframework/ios-arm64/GFPSDKMediationLAN.framework/Headers/GFPSDKMediationLAN.h
@@ -0,0 +1,19 @@
+//
+//  GFPSDKMediationLAN.h
+//
+//  NAM(Naver Ad Manager) SDK for iOS
+//  Copyright 2022-present NAVER Corp.
+//  Unauthorized use, modification and redistribution of this software are strongly prohibited.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GFPSDKMediationLAN.
+FOUNDATION_EXPORT double GFPSDKMediationLANVersionNumber;
+
+//! Project version string for GFPSDKMediationLAN.
+FOUNDATION_EXPORT const unsigned char GFPSDKMediationLANVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GFPSDKMediationLAN/PublicHeader.h>
+
+
